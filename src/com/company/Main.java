d1ea4052607984690fbbac6b492850e2a8d12ed3
Vince Wernsman <vincewernsman@Vinces-MacBook-Pro.local>
2022-02-15 13:39:52 -0700

    Weekly average temp added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,6 +18,22 @@ import java.util.Scanner;
 
                 }
                 System.out.println(dailyAverageTemp[0]);
+                System.out.println(dailyAverageTemp[1]);
+                System.out.println(dailyAverageTemp[2]);
+                System.out.println(dailyAverageTemp[3]);
+                System.out.println(dailyAverageTemp[4]);
+                System.out.println(dailyAverageTemp[5]);
+                System.out.println(dailyAverageTemp[6]);
+
+
+                double [] weeklyAverageTemp = new double[7];
+                double [] weeklyAverageTemp = "0","1", "2", "3", "4", "5", "6"};
+                double weeklyAverageTemp = sum / (dailyAverageTemp.length);
+
+
+
+
+
 
 
 
