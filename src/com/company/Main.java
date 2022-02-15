d5f6a839a269cf348a892fbe1e7701fc3ed35dbc
Vince Wernsman <vincewernsman@Vinces-MacBook-Pro.local>
2022-02-15 10:14:29 -0700

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+        public class Main {
+
+            public static void main(String[] args) {
+
+                double [] dailyAverageTemp = new double[7];
+
+                Scanner userInput = new Scanner (System.in);
+
+
+                for (int i = 0; i < dailyAverageTemp.length; i++) {
+                    System.out.println("Enter a temperature for a day  " + (i + 1) + ": ");
+                    dailyAverageTemp[i] = userInput.nextDouble();
+
+                }
+                System.out.println(dailyAverageTemp[0]);
+
+
+
+
+
+
+
+    }
+}
